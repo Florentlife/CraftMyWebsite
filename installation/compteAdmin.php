810dea395f03478e7553bbec132894cffb3d87e1
Lucerno <lucernopower@gmail.com>
2017-12-06 22:59:57 +0100

    Modification requête SQL vers la nouvelle version

--- installation/compteAdmin.php
+++ installation/compteAdmin.php
@@ -3,10 +3,11 @@ class CompteAdmin
 {
     public function __construct($bdd, $pseudo, $mdp, $email)
     {	
-		$req = $bdd->prepare('INSERT INTO cmw_users(pseudo, mdp, email, anciennete, newsletter, rang, ValidationMail) VALUES(:pseudo, :mdp, :email, :date, 1, 1, 1)');
+		$mot_de_passe = password_hash($mdp, PASSWORD_DEFAULT);
+		$req = $bdd->prepare('INSERT INTO cmw_users (id, pseudo, mdp, email, anciennete, newsletter, rang, tokens, age, skype, resettoken, ip, CleUnique, ValidationMail) VALUES (NULL, :pseudo, :mdp, :email, :date, 0, 1, 0, 0, "", 0, 0, 0, 1)');
 		$req->execute(Array (
 			'pseudo' => $pseudo,
-			'mdp' => password_hash($mdp, PASSWORD_DEFAULT),
+			'mdp' => $mot_de_passe,
 			'email' => $email,
 			'date' => time() 
 		));
