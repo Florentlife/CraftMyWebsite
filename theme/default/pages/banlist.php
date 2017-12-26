69696c2191157cae39fe6ac94d243ffb49eddf59
Sawn <nelox@mail.com>
2017-12-26 18:23:43 +0100

    Modif du banlist.php
    blablabla
    amélioration
    blabla
    truc machin dev blabla c compliqué

--- theme/default/pages/banlist.php
+++ theme/default/pages/banlist.php
@@ -14,17 +14,21 @@
 			<th>Raison</th>
 		</tr>
 		<?php
-		foreach($banlist[0] as $cle => $element)
-		{
-			echo '<tr>';
-			if(!is_string($element))
-				foreach($banlist[0][$cle] as $cle2 => $element2)
-				{
-					echo '<td>'. $element2 .'</td>';
-				}
-			else
-				echo '<td>'.$banlist[0][$cle].'</td><td>Date inconnue</td><td>(Unknown)</td><td>?</td><td>Bannis par un admin</td>';
-			echo '</tr>';
+		if(count($banlist[0]) > 0) {
+			foreach($banlist[0] as $cle => $element)
+			{
+				echo '<tr>';
+				if(!is_string($element))
+					foreach($banlist[0][$cle] as $cle2 => $element2)
+					{
+						echo '<td>'. $element2 .'</td>';
+					}
+				else
+					echo '<td>'.$banlist[0][$cle].'</td><td>Date inconnue</td><td>(Unknown)</td><td>?</td><td>Bannis par un admin</td>';
+				echo '</tr>';
+			}
+		} else {
+			echo '<tr><td colspan="5">Aucun joueurs n\'a été banni à ce jour!</td></tr>';
 		}
 		?>
 	</table>
