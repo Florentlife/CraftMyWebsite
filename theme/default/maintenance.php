e1370e1fb0f146742a2d82f0f35a9f91b71998b0
Florentlife <curvalle.florent@gmail.com>
2018-01-03 13:56:06 +0100

    Intégration de la maintenance
    tout est dans le titre

--- theme/default/maintenance.php
+++ theme/default/maintenance.php
@@ -5,131 +5,77 @@ if($maintenance[$i]['maintenanceEtat'] == 0){
 setTempMess("<script> $( document ).ready(function() { Snarl.addNotification({ title: '', text: 'La maintenance n\'est pas activée !', icon: '<span class=\'glyphicon glyphicon-cog\'></span>'});});</script>");
 header('Location: index.php');
 }
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Maintenance <?php echo $_Serveur_['General']['name']; ?></title>
-	<script src="/theme/<?php echo $_Serveur_['General']['theme']; ?>/js/bootstrap.min.js"></script>
-	<script src="/theme/<?php echo $_Serveur_['General']['theme']; ?>/js/jquery-1.10.2.min.js"></script>
-	<script src="/theme/<?php echo $_Serveur_['General']['theme']; ?>/js/transition.js"></script>
-	<script src="/theme/<?php echo $_Serveur_['General']['theme']; ?>/js/modal.js"></script>
-	<link href="/theme/<?php echo $_Serveur_['General']['theme']; ?>/css/<?php echo $_Serveur_['General']['themeOption']; ?>.css" rel="stylesheet" type="text/css">
-	<link href="/theme/<?php echo $_Serveur_['General']['theme']; ?>/css/style.css" rel="stylesheet" type="text/css">
-	<link href="/theme/<?php echo $_Serveur_['General']['theme']; ?>/css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
-	<style>
-		.form-signin
-		{
-			max-width: 330px;
-			padding: 15px;
-			margin: 0 auto;
-		}
-		.form-signin .form-signin-heading, .form-signin .checkbox
-		{
-			margin-bottom: 10px;
-		}
-		.form-signin .checkbox
-		{
-			font-weight: normal;
-		}
-		.form-signin .form-control
-		{
-			position: relative;
-			font-size: 16px;
-			height: auto;
-			padding: 10px;
-			-webkit-box-sizing: border-box;
-			-moz-box-sizing: border-box;
-			box-sizing: border-box;
-		}
-		.form-signin .form-control:focus
-		{
-			z-index: 2;
-		}
-		.form-signin input[type="text"]
-		{
-			margin-bottom: -1px;
-			border-bottom-left-radius: 0;
-			border-bottom-right-radius: 0;
-		}
-		.form-signin input[type="password"]
-		{
-			margin-bottom: 10px;
-			border-top-left-radius: 0;
-			border-top-right-radius: 0;
-		}
-		.account-wall
-		{
-			margin-top: 20px;
-			padding: 40px 0px 20px 0px;
-			background-color: #f7f7f7;
-			-moz-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-			-webkit-box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-			box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-		}
-		.login-title
-		{
-			color: #555;
-			font-size: 18px;
-			font-weight: 400;
-			display: block;
-		}
-		.profile-img
-		{
-			width: 128px;
-			height: 128px;
-			margin: 0 auto 10px;
-			display: block;
-			-moz-border-radius: 50%;
-			-webkit-border-radius: 50%;
-			border-radius: 20%;
-		}
-		.need-help
-		{
-			margin-top: 10px;
-		}
-		.new-account
-		{
-			display: block;
-			margin-top: 10px;
-		}
-	</style>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="theme/<?php echo $_Serveur_['General']['theme']; ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="theme/<?php echo $_Serveur_['General']['theme']; ?>/css/ionicons.min.css">
+    <link rel="stylesheet" href="theme/<?php echo $_Serveur_['General']['theme']; ?>/css/animate.css">
+    <link rel="stylesheet" href="theme/<?php echo $_Serveur_['General']['theme']; ?>/css/hover.min.css">
+    <link rel="stylesheet" href="theme/<?php echo $_Serveur_['General']['theme']; ?>/css/custom.css">
+    <link rel="stylesheet" href="theme/<?php echo $_Serveur_['General']['theme']; ?>/css/maintenance.css">
 </head>
-<body style="<?php echo $bgType; ?>background-size: cover;">
-	<center style="margin-top: -40px;margin-bottom: -40px;">
-		<img class="spin-maintenance" src="/theme/<?php echo $_Serveur_['General']['theme']; ?>/img/engrenage.png">
-		<img class="spin-maintenance-inverted" src="/theme/<?php echo $_Serveur_['General']['theme']; ?>/img/engrenage.png" style="margin-top: 142px;margin-left: -45px;">
-	</center>
-	<div class="panel panel-primary" style="margin: 0 auto;max-width: 700px;margin-top: 80px;border-radius: 0px;">
-		<div class="panel-heading" style="border-radius: 0px;">
-			<h2 class="panel-title" style="font-family: Minecraftia;text-align: center;font-size: 23px;"></span> <?php echo $_Serveur_['General']['name']; ?> est en maintenance</span></h2>
-		</div>
-		<div class="panel-body" style="text-align: center;font-family: Minecraftia;">
-			<?php echo $maintenance[$i]['maintenanceMsg']; ?>
-		</div>
-		<div class="panel-footer" style="font-family: Minecraftia;"><?php echo $donnees['maintenanceMsgAdmin']; ?> <a data-toggle="modal" data-target="#ConnectionSlide" class="btn btn-primary btn-block" style="margin-top: 10px;border-radius: 0px;"><span class="glyphicon glyphicon-user"></span> Connexion administrateur</a></div>
-	</div>
-	<div class="modal fade" id="ConnectionSlide" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-body">
-					<div class="row">
-						<h4 style="text-align: center;font-family: Minecraftia;font-weight: bold;">Seul les administrateurs peuvent se connecter pour administrer le site !</h4>
-						<form class="form-signin" role="form" method="post" action="?&action=connection">
-							<input type="text" name="pseudo" class="form-control" id="PseudoConectionForm" placeholder="Pseudo" required autofocus>
-							<input type="password" name="mdp" class="form-control" id="MdpConnectionForm" placeholder="Votre mot de passe" required>
-							<button class="btn btn-lg btn-primary btn-block" type="submit"> Connexion</button>
-						</form>
-					</div>
-				</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-primary" data-dismiss="modal">Je ne suis pas administrateur</button>	
-				</div>
-			</div>
-		</div>
-	</div>
+<body>
+	<div class="container">
+        <div class="row">
+            <div class="col-sm-3"></div>
+            <div class="col-sm-6">
+                <div class="card" style="border:0px;">
+                    <h3 class="card-header text-center" style="border:0px;"><i class="fa fa-cog fa-spin"></i> Maintenance</h3>
+                    <hr>
+                    <div class="card-body text-center">
+                        <h5 class="card-title"><?php echo $_Serveur_['General']['name']; ?> reviens très bientôt !</h5>
+                        <h6 class="card-subtitle text-muted">
+                            <?php echo $donnees['maintenanceMsg']; ?>
+                        </h6>
+                    </div>
+                    <hr><?php if($_Joueur_['rang'] != 1 AND $_PGrades_['PermsPanel']['access'] == false)
+                    { ?>
+                    <div class="card-footer text-muted" style="border:0px;">
+                        <a class="btn btn-block btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">Connexion administrateur</a>
+                    </div>
+                </div>
+                <div class="collapse" id="collapseExample"><form method="post" action="?action=connection">
+                    <div class="card card-body text-white bg-dark mb-3" style="border:0px;padding:15px;background:#333;border-top:4px solid #562F91;">
+                        <div class="col-auto">
+                        	<h4><?php echo $donnees['maintenanceMsgAdmin']; ?></h4>
+                            <div class="form-group">
+                                <label class="control-label">Votre pseudonyme</label>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon" style="border:0px;"><i class="fa fa-user"></i></div>
+                                        <input type="text" class="form-control" name="pseudo" id="PSEUDO" placeholder="Pseudonyme" style="border:0px;">
+                                    </div>
+                                </div>
+                                <label class="control-label">Votre mot de passe</label>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon" style="border:0px;"><i class="fa fa-lock"></i></div>
+                                        <input type="password" name="mdp" class="form-control" id="MDP" placeholder="Mot de passe" style="border:0px;">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                	<button class="btn btn-lg btn-primary btn-block" type="submit"> Connexion</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form></div><?php 
+                 }
+                 else
+                 {
+                 	?><div class="card-footer text-muted" style="border: 0px;">
+                 		<a class="btn btn-block btn-primary" href="index.php">Accéder au site</a>
+                 	</div><?php 
+                 }
+                 ?>
+            </div>
+        </div>
+    </div>
 	<div class="panel panel-primary" style="margin: 0 auto;max-width: 700px;margin-top: 80px;border-radius: 0px;">
 		<div class="panel-body">
 		<div class="footer-copyright" style="font-family: Minecraftia;font-weight: 200;text-align: center;font-size: 12px;">
@@ -138,5 +84,12 @@ header('Location: index.php');
 			</div>
 		</div>
 	</div>
+	<script src="theme/<?php echo $_Serveur_['General']['theme']; ?>/js/jquery.min.js"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
+    <script src="theme/<?php echo $_Serveur_['General']['theme']; ?>/js/popper.min.js"></script>
+    <script src="theme/<?php echo $_Serveur_['General']['theme']; ?>/js/bootstrap.min.js"></script>
+    <script src="theme/<?php echo $_Serveur_['General']['theme']; ?>/js/wow.min.js"></script>
+    <script src="theme/<?php echo $_Serveur_['General']['theme']; ?>/js/custom.js"></script>
+    <script src="theme/<?php echo $_Serveur_['General']['theme']; ?>/js/snarl.min.js"></script>
 </body>
 </html>
--- theme/default/preload.php
+++ theme/default/preload.php
@@ -6,15 +6,15 @@ if($maintenance[$i]['maintenanceEtat'] == 1){
 	// On vérifie si le joueur est connecté
 	if(!(isset($_Joueur_))){
 		header('Location: index.php?&redirection=maintenance');
-	} elseif($_Joueur_['rang'] == 1) { // On vérifie si il est admin
+	} elseif($_Joueur_['rang'] == 1 OR $_PGrades_['PermsPanel']['access'] == true) { // On vérifie si il est admin
 		if( $maintenance[$i]['maintenancePref'] == 0 ){ // Si la pref vaut 0 les admins ont accès au site avec l'entête en plus
 		include('theme/' .$_Serveur_['General']['theme']. '/maintenance/entete.php');
 		} elseif ( $maintenance[$i]['maintenancePref'] == 1 ) { // Si la maintenance vaut 1 les admins n'ont pas accès au site mais ils sont redirigés vers le panel admin
 		header('Location: admin.php');
-	}
-		else { // Si le joueur n'est pas admin il est redirigé vers la page de maintenance
-		header('Location: index.php?&redirection=maintenance');
-	}
+		}
+			else { // Si le joueur n'est pas admin il est redirigé vers la page de maintenance
+			header('Location: index.php?&redirection=maintenance');
+		}
 	} else { // Si le joueur n'est pas connecté il est redirigé vers la page de maintenance
 	header('Location: index.php?&redirection=maintenance');
 }
