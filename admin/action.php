c9fb96269fa9b1b21bba56b6d906c1a6bc94eb86
cercus <dorian.bourdontpe@gmail.com>
2018-01-05 12:52:30 +0100

    Add files via upload

--- admin/action.php
+++ admin/action.php
@@ -10,49 +10,31 @@
 		require_once('admin/actions/commande.php');
 		break;
 
-		case 'addPrefix':
-			require_once('admin/actions/addPrefix.php');
-			$_SESSION['referrerAdmin'] = 'forum';
-		break;
-
-		case 'supprPrefix':
-			require_once('admin/actions/supprPrefix.php');
-			$_SESSION['referrerAdmin'] = 'forum';
-		break;
-
-		case 'configTheme':
-			require_once('theme/'.$_Serveur_['General']['theme'].'/config/configAdminTraitement.php');
-			$_SESSION['referrerAdmin'] = 'theme';
-		break;
-
-		case 'addSmiley':
-			require_once('admin/actions/addSmiley.php');
-			$_SESSION['referrerAdmin'] = 'forum';
-		break;
-
-		case 'supprSmiley':
-			require_once('admin/actions/supprSmiley.php');
-			$_SESSION['referrerAdmin'] = 'forum';
-		break;
-
 		case 'creerCoupon':
 			require_once('admin/actions/creerCoupon.php');
-			$_SESSION['referrerAdmin'] = 'boutique';
+		break;
+
+		case 'creerPostit':
+			require_once('admin/actions/creerPostit.php');
+			$_SESSION['referrerAdmin'] = 'accueil';
+		break;
+
+		case 'supprPostit':
+			require_once('admin/actions/supprPostit.php');
+			$_SESSION['referrerAdmin'] = 'accueil';
 		break;
 
 		case 'supprCoupon':
 			require_once('admin/actions/supprCoupon.php');
-			$_SESSION['referrerAdmin'] = 'boutique';
 		break;
 
 		case 'ajout_favicon':
 			require_once('admin/actions/ajout_favicon.php');
-			$_SESSION['referrerAdmin'] = 'configsite';
 		break;
 		
 		case 'general': 
 		require_once('admin/actions/general.php');
-			$_SESSION['referrerAdmin'] = 'configsite';
+		$_SESSION['referrerAdmin'] = 'general';
 		break;
 		
 		case 'editTheme': 
@@ -72,12 +54,12 @@
 		
 		case 'creerPage': 
 		require_once('admin/actions/creerPage.php');
-		$_SESSION['referrerAdmin'] = 'custompages';
+		$_SESSION['referrerAdmin'] = 'pages';
 		break;
 		
 		case 'supprPage': 
 		require_once('admin/actions/supprPage.php');
-		$_SESSION['referrerAdmin'] = 'custompages';
+		$_SESSION['referrerAdmin'] = 'pages';
 		break;
 		
 		case 'boutique': 
@@ -102,17 +84,17 @@
 		
 		case 'serveurJsonNew': 
 		require_once('admin/actions/serveurJsonNew.php');
-		$_SESSION['referrerAdmin'] = 'reglagejsonapi';
+		$_SESSION['referrerAdmin'] = 'regserv';
 		break;
 		
 		case 'serveurConfig': 
 		require_once('admin/actions/serveurConfig.php');
-		$_SESSION['referrerAdmin'] = 'reglagejsonapi';
+		$_SESSION['referrerAdmin'] = 'regserv';
 		break;
 		
 		case 'supprJson': 
 		require_once('admin/actions/serveurJsonSuppr.php');
-		$_SESSION['referrerAdmin'] = 'reglagejsonapi';
+		$_SESSION['referrerAdmin'] = 'regserv';
 		break;
 		
 		case 'newLienMenu': 
@@ -122,22 +104,22 @@
 		
 		case 'editPayement': 
 		require_once('admin/actions/editPayement.php');
-		$_SESSION['referrerAdmin'] = 'paiement';
+		$_SESSION['referrerAdmin'] = 'payement';
 		break;
 		
 		case 'creerOffrePaypal': 
 		require_once('admin/actions/creerOffrePaypal.php');
-		$_SESSION['referrerAdmin'] = 'paiement';
+		$_SESSION['referrerAdmin'] = 'payement';
 		break;
 		
 		case 'modifierOffrePaypal': 
 		require_once('admin/actions/modifierOffrePaypal.php');
-		$_SESSION['referrerAdmin'] = 'paiement';
+		$_SESSION['referrerAdmin'] = 'payement';
 		break;
 		
 		case 'supprimerPaypalOffre': 
 		require_once('admin/actions/supprimerPaypalOffre.php');
-		$_SESSION['referrerAdmin'] = 'paiement';
+		$_SESSION['referrerAdmin'] = 'payement';
 		break;
 		
 		case 'supprLienMenu': 
@@ -177,7 +159,7 @@
 		
 		case 'postNavRap': 
 		require_once('admin/actions/postNavRap.php');
-		$_SESSION['referrerAdmin'] = 'slidemini';
+		$_SESSION['referrerAdmin'] = 'accueil';
 		break;
 
 		case 'postNews': 
@@ -207,27 +189,27 @@
 		
 		case 'editRapNav': 
 		require_once('admin/actions/editRapNav.php');
-		$_SESSION['referrerAdmin'] = 'slidemini';
+		$_SESSION['referrerAdmin'] = 'accueil';
 		break;
 		
 		case 'newSlider':
 		require_once('admin/actions/newSlider.php');
-		$_SESSION['referrerAdmin'] = 'slidemini';
+		$_SESSION['referrerAdmin'] = 'accueil';
 		break;
 		
 		case 'changeSlider':
 		require_once('admin/actions/changeSlider.php');
-		$_SESSION['referrerAdmin'] = 'slidemini';
+		$_SESSION['referrerAdmin'] = 'accueil';
 		break;
 		
 		case 'postSlider':
 		require_once('admin/actions/postSlider.php');
-		$_SESSION['referrerAdmin'] = 'slidemini';
+		$_SESSION['referrerAdmin'] = 'accueil';
 		break; 
 		
 		case 'supprSlider':
 		require_once('admin/actions/supprSlider.php');
-		$_SESSION['referrerAdmin'] = 'slidemini';
+		$_SESSION['referrerAdmin'] = 'accueil';
 		break; 
 		
 		case 'postBG':
@@ -240,34 +222,38 @@
 		$_SESSION['referrerAdmin'] = 'theme';
 		break; 
 		
-		case 'creerLienVote':
-		require_once('admin/actions/creerLienVote.php');
-		$_SESSION['referrerAdmin'] = 'voter';
+		case 'modifierVotesGen':
+		require_once('admin/actions/modifierVotesGen.php');
+		$_SESSION['referrerAdmin'] = 'votes';
 		break;
 		
-		case 'supprVote':
-		require_once('admin/actions/supprVote.php');
-		$_SESSION['referrerAdmin'] = 'voter';
+		case 'creerLienVote':
+		require_once('admin/actions/creerLienVote.php');
+		$_SESSION['referrerAdmin'] = 'votes';
+		break;
+		
+		case 'supprLienVote':
+		require_once('admin/actions/supprLienVote.php');
+		$_SESSION['referrerAdmin'] = 'votes';
 		break;
 		
 		case 'editPage':
 		require_once('admin/actions/editPage.php');
-		$_SESSION['referrerAdmin'] = 'custompages';
+		$_SESSION['referrerAdmin'] = 'pages';
 		break;
 		
 		case 'creerSection':
 		require_once('admin/actions/creerSection.php');
-		$_SESSION['referrerAdmin'] = 'custompages';
+		$_SESSION['referrerAdmin'] = 'pages';
 		break;
 		
 		case 'supprSection':
 		require_once('admin/actions/supprSection.php');
-		$_SESSION['referrerAdmin'] = 'custompages';
+		$_SESSION['referrerAdmin'] = 'pages';
 		break;
 		
 		case 'editPermissions':
 		require_once('admin/actions/editPermissions.php');
-		$_SESSION['referrerAdmin'] = 'grade';
 		break;
 		
 		case 'supprTicket':
@@ -303,7 +289,7 @@
 		case 'resetVotes':
 		if($_Joueur_['rang'] == 1 OR $_PGrades_['PermsPanel']['vote']['actions']['resetVote'] == true)
 			$bddConnection->exec('DELETE FROM cmw_votes');
-		$_SESSION['referrerAdmin'] = 'voter';
+		$_SESSION['referrerAdmin'] = 'votes';
 		break;
 		
 		case 'etatTickets':
@@ -333,47 +319,47 @@
 
 		case 'commandeConsole': 
 		require_once('admin/actions/commandeConsole.php');
-		$_SESSION['referrerAdmin'] = 'accueil';
+		$_SESSION['referrerAdmin'] = 'informations';
 		break;
 
 		case 'commandeRechargementPlugins': 
 		require_once('admin/actions/commandeRechargementPlugins.php');
-		$_SESSION['referrerAdmin'] = 'accueil';
+		$_SESSION['referrerAdmin'] = 'informations';
 		break;
 
 		case 'commandeRedemarrageServer': 
 		require_once('admin/actions/commandeRedemarrageServer.php');
-		$_SESSION['referrerAdmin'] = 'accueil';
+		$_SESSION['referrerAdmin'] = 'informations';
 		break;
 
 		case 'switchSysMail': 
 		require_once('admin/actions/switchSysMail.php');
-		$_SESSION['referrerAdmin'] = 'accueil';
+		$_SESSION['referrerAdmin'] = 'informations';
 		break;
 
 		case 'editSysMail': 
 		require_once('admin/actions/editSysMail.php');
-		$_SESSION['referrerAdmin'] = 'accueil';
+		$_SESSION['referrerAdmin'] = 'informations';
 		break;
 
 		case 'editNbrPerIP': 
 		require_once('admin/actions/editNbrPerIP.php');
-		$_SESSION['referrerAdmin'] = 'accueil';
+		$_SESSION['referrerAdmin'] = 'informations';
 		break;
 
 		case 'supprGrade': 
 		require_once('admin/actions/supprGrade.php');
-		$_SESSION['referrerAdmin'] = 'grade';
+		$_SESSION['referrerAdmin'] = 'grades';
 		break;
 
 		case 'addGrade': 
 		require_once('admin/actions/addGrade.php');
-		$_SESSION['referrerAdmin'] = 'grade';
+		$_SESSION['referrerAdmin'] = 'grades';
 		break;
 
 		case 'editGrade': 
 		require_once('admin/actions/editGrade.php');
-		$_SESSION['referrerAdmin'] = 'grade';
+		$_SESSION['referrerAdmin'] = 'grades';
 		break;
 		
 		// Si le joueur a rentré un url contenant une valeur d'action innexistant?
@@ -381,8 +367,5 @@
 		header('Location: admin.php');
 	}
 }
-if(isset($_SESSION['referrerAdmin']))
-	header('Location: admin.php?page='.$_SESSION['referrerAdmin']);
-else
-	header('Location: admin.php');
+header('Location: admin.php');
 ?>
