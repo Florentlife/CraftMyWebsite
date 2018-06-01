3cc578f9ba6c8ea8de1dcc7e153c79151dba8cf4
Ethanox <25269808+EthanoxXx@users.noreply.github.com>
2018-06-01 10:35:23 +0200

    UPDATE 1.7 (ID 46)
    ID 46: Pour les admins quand une mise a jour est arrivé un message s'affiche sur la page d'accueil pour pouvoir mettre a jour !
    j'ai fait via perm car les nom des grades sont customisable. Je me suis basé sur le fait que la personne qui peut voir la page "update" dans le panel, peut voir la notif

--- theme/default/index.php
+++ theme/default/index.php
@@ -35,6 +35,14 @@ require('theme/'. $_Serveur_['General']['theme'] . '/config/configTheme.php');?>
 			?>
 			<?php tempMess(); ?>
 		<?php
+		include("./include/version.php");
+        include("./include/version_distant.php");
+        if($versioncms != $versioncmsrelease && ($_Joueur_['rang'] == 1 OR $_PGrades_['PermsPanel']['update']['showPage'] == 'on')) {?>
+        <div class="alert alert-warning alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert">&times;</button>
+            Une mise à jour est disponible (<a href="https://craftmywebsite.fr/telecharger" target="_blank" class="alert-link"><?= $versioncmsrelease?></a>)
+        </div>
+        <?php }
 		$check_installation_dossier = "installation";
 		if (is_dir($check_installation_dossier)) { ?>
 		<header class="heading-pagination">
