4831dd554e5cc61a9ec75840132bae35e7cedc46
Ethanox <mail.ethanox@gmail.Com>
2018-06-14 00:52:42 +0200

    UPDATE 1.7 (ID 109)
    ID 109: Ranger les membres par odres alphabétique par default
    
    Je sais pas si c'est cette liste

--- admin/donnees/membres.php
+++ admin/donnees/membres.php
@@ -1,6 +1,6 @@
 <?php
 if($_Joueur_['rang'] == 1 OR $_PGrades_['PermsPanel']['members']['showPage'] == true) { 
-	$membresReq = $bddConnection->query('SELECT * FROM cmw_users ORDER BY email');
+	$membresReq = $bddConnection->query('SELECT * FROM cmw_users ORDER BY pseudo');
 
 	$i = 0;
 	while($membresDonnees = $membresReq->fetch(PDO::FETCH_ASSOC))
