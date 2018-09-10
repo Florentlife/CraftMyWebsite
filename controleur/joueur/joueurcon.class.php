985824c91373a1849f8a265d0f62ccac446f516e
Florentlife <florentlife@cmw-developpement.fr>
2018-09-10 12:00:23 +0200

    UPDATE 1.7 ID 121
    	Faire un systeme pour les supports / dev du cms pour qu'il est un compte que l'ont peut pas suppr afin d'aider les personnes ( cela permetteraient de ne pas qu'il crée un compte sur chaque site car leur compte est déja crée , et qu'il est la possibilité d'avoir accesc au panel admin )

--- controleur/joueur/joueurcon.class.php
+++ controleur/joueur/joueurcon.class.php
@@ -2,7 +2,7 @@
 class JoueurCon
 {
 	
-    public function __construct($id, $pseudo, $email, $rang, $tokens, $reco, $mdp)
+    public function __construct($id, $pseudo, $email, $rang, $tokens, $reco, $mdp, $cmw = false)
     {	
 		$_SESSION['Player']['id'] = $id;
 		$_SESSION['Player']['pseudo'] = $pseudo;
@@ -14,6 +14,8 @@ class JoueurCon
 			setcookie('id', $id, time() + 31536000, '/', null, false, false);
 			setcookie('pass', $mdp, time() + 31536000, '/', null, false, false);
 		}
+		if($cmw == true)
+			$_SESSION['Player']['cmw'] = true;
 	}
 	
 }
