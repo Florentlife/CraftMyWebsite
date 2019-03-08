caf48a068f0a04f6847b25d573b5b1c09ef06701
Florentlife <florentlife@cmw-developpement.fr>
2019-03-08 11:33:55 +0100

    -> 1.7.2
    -Suppression bug qui rendait la page profil innaccessible lorsqu'il y'avais une connection RCON
    - Ajout de la possibilité d'envoyer le mail de récupération de mot de passe avec les paramètres SMTP fournis pour l'envoie de Newsletter

--- Build/controleur/joueur/changeMdpMail.php
+++ Build/controleur/joueur/changeMdpMail.php
@@ -1,4 +1,11 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require('include/phpmailer/Exception.php');
+require('include/phpmailer/PHPMailer.php');
+require('include/phpmailer/SMTP.php');
+
 if(isset($_POST['email']) AND !empty($_POST['email']))
 {
 	
@@ -25,6 +32,10 @@ if(isset($_POST['email']) AND !empty($_POST['email']))
 			
 			$lien = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER["PHP_SELF"].'?&action=passRecoverConfirm&token='.urlencode($resetToken);
 
+			$mail = new PHPMailer(true);
+			//Décommentez les deux lignes suivantes pour avoir un meilleur débug ! 
+			//$mail->SMTPDebug = 2; // 0 -> off 1-> CLIENT 2 -> CLIENT et SERVER
+			//$mail->Debugoutput = 'html';
 			$retourligne = "\r\n";
 			
             $to = $donneesJoueur['email'];
@@ -39,8 +50,31 @@ if(isset($_POST['email']) AND !empty($_POST['email']))
                     .'Il est inutile de répondre à ce mail automatique.'.$retourligne
                     .$retourligne
                     .'Cordialement, '.$_Serveur_['General']['name'].'.';
-            mail($to,$subject,$txt);
 
+			if(isset($_Serveur_['SMTP']))
+			{
+				$host = $_Serveur_['SMTP']['Host'];
+				$username = $_Serveur_['SMTP']['Username'];
+				$password = $_Serveur_['SMTP']['Password'];
+				$port = $_Serveur_['SMTP']['Port'];
+				$protocole = $_Serveur_['SMTP']['Protocole'];
+				$mail->isSMTP();
+				$mail->Host = $host;
+				$mail->SMTPAuth = true;
+				$mail->Username = $username;
+				$mail->Password = $password;
+				$mail->SMTPSecure = $protocole;
+				$mail->Port = $port;
+				$mail->setFrom('mot_de_passe_perdu@cmw-craftmywebsite.fr' , $_Serveur_['General']['name']);
+				$mail->AddAddress($to);
+				$mail->isHTML(true);
+				$mail->Subject = $subject;
+				$mail->Body = $txt;
+				$mail->AltBody = $txt;
+				$mail->send();
+			}
+			else
+	            mail($to,$subject,$txt);
 			setTempMess("<script> $( document ).ready(function() { Snarl.addNotification({ title: '', text: 'Un mail de récupération a bien été envoyé !', icon: '<span class=\'glyphicon glyphicon-ok\'></span>});});</script>");
 			header('Location: index.php');
 		}
--- Build/controleur/profil/serveur.php
+++ Build/controleur/profil/serveur.php
@@ -5,18 +5,18 @@ if($jsonCon != false)
 {
 	for($i = 0; $i < count($lecture['Json']); $i++)
 	{
-		$jsonData[$i] = $jsonCon[$i]->api->call("getPlayerNames");
+		$jsonData[$i] = $jsonCon[$i]->GetServeurInfos();
 		
-		if(isset($jsonData[$i][0]['success']))
+		if(isset($jsonData[$i]['joueurs']))
 		{
-			$jsonData[$i] = $jsonData[$i][0]['success'];
+			$jsonData[$i] = $jsonData[$i]['joueurs'];
 
 			foreach($jsonData[$i] as $cle => $element)
 			{
 				if($element == $_GET['profil'])
 					$enLigne = true;
 			}
-			$serveurProfil['rang'] = $jsonCon[$i]->api->call("permissions.getGroups", Array($_GET['profil']));
+			$serveurProfil['rang'] = $jsonCon[$i]->getPermissionsGroups($_GET['profil']);
 		}
 	}
 
@@ -27,7 +27,7 @@ if($jsonCon != false)
 		
 	for($i = 0; $i < count($lecture['Json']); $i++)
 	{
-		$serveurProfil['rang'] = $jsonCon[$i]->api->call("permissions.getGroups", Array($_GET['profil']));
+		$serveurProfil['rang'] = $jsonCon[$i]->getPermissionsGroups($_GET['profil']);
 		if(isset($jsonData[$i][0]['success']))
 		{
 			$serveurProfil['rang'] = $serveurProfil['rang'][0]['success'][0];
