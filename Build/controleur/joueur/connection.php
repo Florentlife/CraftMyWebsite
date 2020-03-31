0c15227baf91cf24b7f5a00ff5c8156220dbfa7a
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2020-03-31 02:17:40 +0200

    Update connection and ban perm

--- Build/controleur/joueur/connection.php
+++ Build/controleur/joueur/connection.php
@@ -37,17 +37,13 @@ if(isset($_POST['pseudo']) AND isset($_POST['mdp']) AND !empty($_POST['pseudo'])
 							$_PGrades_ = $gradeLecture->GetTableau();
 						}
 						else
+						{
 							$_PGrades_ = false;
+						}
 					}
 					if($donneesJoueur['rang'] == 1 OR $_PGrades_['PermsPanel']['access'] == true) { 
 						$utilisateur_connection = new JoueurCon($donneesJoueur['id'], $donneesJoueur['pseudo'], $donneesJoueur['email'], $donneesJoueur['rang'], $donneesJoueur['tokens'], $reconnexion, $donneesJoueur['mdp']);
-						if( $maintenance[$i]['maintenancePref'] == 0 ){ // Si la pref vaut 0 les admins ont accès au site avec l'entête en plus
-							include('theme/' .$_Serveur_['General']['theme']. '/maintenance/entete.php');
-						} elseif ( $maintenance[$i]['maintenancePref'] == 1 ) { // Si la maintenance vaut 1 les admins n'ont pas accès au site mais ils sont redirigés vers le panel admin
-							header('Location: admin.php');
-						}else { // Si le joueur n'est pas admin il est redirigé vers la page de maintenance
-							header('Location: index.php?&redirection=maintenance');
-						}
+						header('Location: index.php?&redirection=maintenance');
 					} else { // Si le joueur n'est pas connecté il est redirigé vers la page de maintenance
 						header('Location: index.php?&redirection=maintenance');
 					}
