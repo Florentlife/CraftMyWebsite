ac02700fd0b2db8f7762878c8759bd04abe8fbff
Emilien52 <40137638+Emilien52@users.noreply.github.com>
2020-05-23 11:33:29 +0200

    Ajout contributeurs
    Les contributeurs depuis la 1.7.0

--- Build/include/note_dev.php
+++ Build/include/note_dev.php
@@ -1,3 +1,15 @@
 <?php
 $notedevdistant = file_get_contents('http://craftmywebsite.fr/release/note.txt');
 ?>
+
+Contributeurs:
+
+1.7.0: Florentlife, Oxykis et Ethanox
+
+1.7.1: Florentlife et Emilien52
+
+1.7.2: Ethanox, PinglsDzn, Emilien52, Mario359, Guedesite et Florentlife
+
+1.7.3: Emilien52, Guedesite, BadiiiiX et Florentlife
+
+1.8.0: Guedesite, PinglsDzn, Emilien52, BadiiiX, Pandax et Florentlife
