86b2e36592728d825c50fe30a93060285e2c3131
Emilien52 <40137638+Emilien52@users.noreply.github.com>
2020-06-11 16:23:01 +0200

    Maj données + ajouts dossier asset

---
+++ Build/admin_18/assets/js/main.js
@@ -0,0 +1,26 @@
+// Toastr Notifications
+toastr.options = {
+    "closeButton"   : true,
+    "debug"         : true,
+	"newestOnTop"      : false,
+	"progressBar"      : false,
+	"positionClass"    : "toast-top-right",
+	"preventDuplicates": false,
+	"onclick"          : null,
+	"showDuration"     : "500",
+	"hideDuration"     : "500",
+	"timeOut"          : "5000",
+	"extendedTimeOut"  : "1000",
+	"showEasing"       : "swing",
+	"hideEasing"       : "linear",
+	"showMethod"       : "fadeIn",
+	"hideMethod"       : "fadeOut"
+}
+
+// Loader
+$(document).ready(function() {
+	setTimeout(function(){
+		document.getElementsByTagName('body')[0].className = 'loaded';
+	}, 100);
+	
+});
