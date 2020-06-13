16dd77bac1c62bbf0741569846317c1a2f25923c
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2020-06-13 23:55:38 +0200

    oups

--- Build/admin_18/actions/getJsonAchat.php
+++ Build/admin_18/actions/getJsonAchat.php
@@ -2,9 +2,9 @@
 {
 
 if($_POST['axe'] == 'pseudo') {
-	$boutiqueListeReq = $bddConnection->prepare('SELECT cmw_boutique_stats.id as id, cmw_boutique_stats.id AS \'id2\', cmw_boutique_stats.prix AS prixTotal, cmw_boutique_offres.prix AS prix, cmw_boutique_stats.pseudo AS pseudo, cmw_boutique_offres.nom AS titre, cmw_boutique_stats.date_achat AS date_achat FROM cmw_boutique_stats INNER JOIN cmw_boutique_offres ON offre_id = cmw_boutique_offres.id WHERE pseudo LIKE \'%:search%\' ORDER BY :axe :axetype');
+	$boutiqueListeReq = $bddConnection->prepare('SELECT cmw_boutique_stats.id as id, cmw_boutique_stats.id AS \'id2\', cmw_boutique_stats.prix AS prixTotal, cmw_boutique_offres.prix AS prix, cmw_boutique_stats.pseudo AS pseudo, cmw_boutique_offres.nom AS titre, cmw_boutique_stats.date_achat AS date_achat FROM cmw_boutique_stats INNER JOIN cmw_boutique_offres ON offre_id = cmw_boutique_offres.id WHERE pseudo LIKE \':search\' ORDER BY :axe :axetype');
 		$boutiqueListeReq->execute(array(
-			'search' => $_POST['search'],
+			'search' => '%'.$_POST['search'].'%',
 			'axe' => $_POST['axe'],
 			'axetype' => $_POST['axeType']
 		));
